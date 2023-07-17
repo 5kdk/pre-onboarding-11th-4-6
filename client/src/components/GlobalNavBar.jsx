43d19110b6bd401b6ef0d5d52dcdf63f55f8499e
5kdk <5kdk.code@gmail.com>
2023-07-17 23:46:24 +0900

    feat: GNB 컴포넌트 구현

--- client/src/components/GlobalNavBar.jsx
+++ client/src/components/GlobalNavBar.jsx
@@ -1,5 +1,25 @@
-const GlobalNavBar = () => {
-  return <header></header>;
-};
+import { Flex, Header, Image, Text } from '@mantine/core';
+import logo from '../assets/logo.svg';
 
+const GlobalNavBar = () => (
+  <Header withBorder={false}>
+    <Flex maw={1000} m={'auto'} px={20} align={'center'} justify={'space-between'}>
+      <Image src={logo} maw={126} />
+      <Flex fw={700}>
+        <Text px={'lg'} py={'md'}>
+          소개
+        </Text>
+        <Text px={'lg'} py={'md'}>
+          질문과 답변
+        </Text>
+        <Text px={'lg'} py={'md'}>
+          소식받기
+        </Text>
+        <Text px={'lg'} py={'md'}>
+          제휴/문의
+        </Text>
+      </Flex>
+    </Flex>
+  </Header>
+);
 export default GlobalNavBar;
