338f1f8aa3ab76dad94ba6ed31612af17a0df3ec
5kdk <5kdk.code@gmail.com>
2023-07-19 20:40:04 +0900

    feat: 하이라이팅 기능구현

--- client/src/components/Suggestion.jsx
+++ client/src/components/Suggestion.jsx
@@ -1,9 +1,9 @@
-import { forwardRef } from 'react';
+import { forwardRef, Fragment } from 'react';
 import { Box, List, Loader, ScrollArea, Text } from '@mantine/core';
 import { IconSearch } from '@tabler/icons-react';
 import { useRecommendedSearchTerms } from '../hooks';
 
-const Wrapper = forwardRef(({ children }, ref) => (
+const Wrapper = forwardRef(({ align, children }, ref) => (
   <Box
     ref={ref}
     pos="absolute"
@@ -12,7 +12,7 @@ const Wrapper = forwardRef(({ children }, ref) => (
     p="lg"
     bg="#fff"
     sx={theme => ({
-      textAlign: 'left',
+      textAlign: align || 'left',
       borderRadius: theme.radius.lg,
       boxShadow: theme.shadows.md,
     })}>
@@ -20,42 +20,55 @@ const Wrapper = forwardRef(({ children }, ref) => (
   </Box>
 ));
 
+const HighlightSearchTerm = ({ searchTerm, sickNm }) => {
+  const regex = new RegExp(`(${searchTerm})`, 'gi');
+  const parts = sickNm.split(regex);
+
+  return (
+    <>
+      {parts.map((part, index) => (
+        <Fragment key={index}>
+          {part.toLowerCase() === searchTerm.toLowerCase() ? <strong>{part}</strong> : part}
+        </Fragment>
+      ))}
+    </>
+  );
+};
+
 const Suggestion = forwardRef(({ searchTerm }, ref) => {
   const { recommendations, isLoading, error } = useRecommendedSearchTerms(searchTerm);
 
   if (isLoading) {
-    <Wrapper ref={ref}>
-      <Loader />
-    </Wrapper>;
+    return (
+      <Wrapper ref={ref} align="center">
+        <Loader />
+      </Wrapper>
+    );
   }
 
   if (error) {
-    <Wrapper ref={ref}>요청사항을 처리하는데 실패했습니다.</Wrapper>;
+    return <Wrapper ref={ref}>요청사항을 처리하는데 실패했습니다.</Wrapper>;
   }
 
   return (
     <Wrapper ref={ref}>
       <Text size="sm" mb="md">
-        추천 검색어
+        {recommendations.length !== 0 ? '추천 검색어' : '검색어 없음'}
       </Text>
-      {recommendations.length === 0 ? (
-        <Text>검색어 없음</Text>
-      ) : (
-        <ScrollArea h={300}>
-          <List spacing="sm" sx={{ cursor: 'pointer' }} icon={<IconSearch size="1rem" color="lightgray" />}>
-            {recommendations.map(({ sickCd, sickNm }) => (
-              <List.Item key={sickCd} tabIndex="0">
-                {sickNm}
-              </List.Item>
-            ))}
-          </List>
-        </ScrollArea>
-      )}
+      <ScrollArea h={300}>
+        <List spacing="sm" sx={{ cursor: 'pointer' }} icon={<IconSearch size="1rem" color="lightgray" />}>
+          {recommendations.map(({ sickCd, sickNm }) => (
+            <List.Item key={sickCd} tabIndex="0">
+              {HighlightSearchTerm({ sickNm, searchTerm })}
+            </List.Item>
+          ))}
+        </List>
+      </ScrollArea>
     </Wrapper>
   );
 });
 
 Suggestion.displayName = 'Suggestion';
-Wrapper.displayName = 'Popup';
+Wrapper.displayName = 'Wrapper';
 
 export default Suggestion;
