470792d66e43e6b7f40169f233d7c911e9a763ca
5kdk <5kdk.code@gmail.com>
2023-07-19 20:40:33 +0900

    style: props 중괄호 제거

--- client/src/components/Banner.jsx
+++ client/src/components/Banner.jsx
@@ -3,16 +3,8 @@ import { SearchBar } from '.';
 
 const Banner = () => {
   return (
-    <Box bg={'#CAE9FF'}>
-      <Flex
-        direction={'column'}
-        align={'center'}
-        justify={'center'}
-        ta={'center'}
-        maw={1040}
-        m={'auto'}
-        pt={80}
-        pb={160}>
+    <Box bg="#CAE9FF">
+      <Flex direction="column" align="center" justify="center" ta="center" maw={1040} m="auto" pt={80} pb={160}>
         <Title order={2} fz={36} mb={40}>
           국내 모든 임상시험 검색하고
           <br />
