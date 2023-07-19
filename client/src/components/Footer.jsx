3e86d79cd3431648486b964d378b4af086fcfcf2
5kdk <5kdk.code@gmail.com>
2023-07-19 22:58:25 +0900

    refactor: return 문 정리

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,15 +1,13 @@
 import { Flex, Footer as MantineFooter, Text } from '@mantine/core';
 
-const Footer = () => {
-  return (
-    <MantineFooter py={40} bg="#2d3d50">
-      <Flex direction="column" maw={1040} m="auto" c="#A7AFB7">
-        <Text mb={16}>원티드 프리온보딩 인턴십</Text>
-        <Text fz={14}>4주차 기업 과제</Text>
-        <Text fz={14}>검색창 구현, 검색어 추천 기능 구현, 캐싱 기능 구현하기</Text>
-      </Flex>
-    </MantineFooter>
-  );
-};
+const Footer = () => (
+  <MantineFooter py={40} bg="#2d3d50">
+    <Flex direction="column" maw={1040} m="auto" c="#A7AFB7">
+      <Text mb={16}>원티드 프리온보딩 인턴십</Text>
+      <Text fz={14}>4주차 기업 과제</Text>
+      <Text fz={14}>검색창 구현, 검색어 추천 기능 구현, 캐싱 기능 구현하기</Text>
+    </Flex>
+  </MantineFooter>
+);
 
 export default Footer;
